3ee27fc920fb9bc08f48d8c6f0f8bc602a765cbe
thejasree17069-dot <thejasree17069@gmail.com>
2026-07-20 18:28:30 +0530

    Added script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "Something I've Never Told You...";
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 80);
+  }
+}
+
+window.onload = typeWriter;
